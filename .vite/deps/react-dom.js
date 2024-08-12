77a6f4c4a98ffbbe44de0d8e99a0e3848f30e984
Juhasz Roland <jszrl2003@gmail.com>
2024-08-12 19:26:51 +0200

    run index

---
+++ .vite/deps/react-dom.js
@@ -0,0 +1,6 @@
+import {
+  require_react_dom
+} from "./chunk-TAMLLQRQ.js";
+import "./chunk-Q34MU4L5.js";
+export default require_react_dom();
+//# sourceMappingURL=react-dom.js.map
---
+++ .vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-Q34MU4L5.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
