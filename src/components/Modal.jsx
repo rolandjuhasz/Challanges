1a97e4c828d5b0bf9658cb42ae66ef2a4430ecef
Juhasz Roland <jszrl2003@gmail.com>
2024-08-13 22:10:50 +0200

    add full animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,21 @@
 import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
-const hiddenAnimationState = {opacity: 0, y: 30};
+
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog 
-
-        initial={hiddenAnimationState}
-        animate={{opacity: 1, y: 0}}
-        exit={{opacity: 0, y: 30}}
-        open className="modal">
+        variants={{
+          hidden: {opacity: 0, y: 30},
+          visible: {opacity: 1, y: 0},
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        open 
+        className="modal">
         <h2>{title}</h2>
         {children}
       </motion.dialog>
