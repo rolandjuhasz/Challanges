eacca07c4a9eabc731280e7321a6c58d74e5fc15
Juhasz Roland <jszrl2003@gmail.com>
2024-08-14 15:59:28 +0200

    add some challange

---
+++ data.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+// data.js
+export const EXAMPLES = [
+  { title: "Easy challange", 
+    description: "The weekly tasks are displayed here" 
+  },
+
+  { title: "Medium challange", 
+    description: "The weekly tasks are displayed here" 
+  },
+
+  { title: "Hard challange", 
+    description: "The weekly tasks are displayed here" 
+  }
+];
